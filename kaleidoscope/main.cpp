e5ddce632ab22d34d351740fdf5ea132c4de9e8f
Kieran <kcosson111@gmail.com>
2022-06-13 22:03:40 +0100

    Fix build failing when using LLVM's verifyFunction(...)

--- kaleidoscope/main.cpp
+++ kaleidoscope/main.cpp
@@ -4,10 +4,20 @@
 #include <memory>
 #include <map>
 
-#include "llvm/ADT/APFloat.h"
-
 // #include "compiler/tokens.cpp"
 #include "compiler/parsing.cpp"
+
+// As I understand it, there was an errant (unlisted) breaking change for visual c++ and a specific api.
+// This tells the linker that there is a new name for the two functions changed.
+// More info: https://github.com/conan-io/conan-center-index/issues/4826
+// Also: https://developercommunity.visualstudio.com/t/-imp-std-init-once-complete-unresolved-external-sy/1684365#T-N10041864
+// Also: https://developercommunity.visualstudio.com/t/-imp-std-init-once-complete-unresolved-external-sy/1684365#T-N10047968
+// The more complete fix is apparently to rebuild the library (llvm, I think) with a newer 'toolset' 
+// (i.e. when using CMake to build llvm, use the latest visual c++ and visual studio setup)
+#if _MSC_VER >= 1932 // Visual Studio 2022 version 17.2+
+#    pragma comment(linker, "/alternatename:__imp___std_init_once_complete=__imp_InitOnceComplete")
+#    pragma comment(linker, "/alternatename:__imp___std_init_once_begin_initialize=__imp_InitOnceBeginInitialize")
+#endif
 
 int main() {
     //tokens::interactive();
